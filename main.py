2dc62447cae607453cd952d5a56e63f6641cd6ad
Wyatt Priddy <wyattpriddy50@gmail.com>
2023-10-24 19:56:11 -0400

    initial commit

---
+++ main.py
@@ -0,0 +1,4 @@
+from random import randint
+
+def function():
+    print('hello world {}'.format(randint(0, 10)))
---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='M09 In Class Activity',
+      version='1.0',
+      description='Python Distribution Utilities',
+      author='Wyatt Priddy',
+      author_email='wpriddy@tds8tv.edu',
+      packages=['random'],
+     )
